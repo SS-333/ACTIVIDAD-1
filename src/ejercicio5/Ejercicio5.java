21edc0bb30614ee7c1c92e0467bd42d45bbb8cbf
Sergio Romero <seromeros@unal.edu.co>
2023-03-04 18:56:41 -0500

    Se agrego el Ejercicio-5

---
+++ src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,15 @@
+package ejercicio5;
+import java.lang.Math;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        
+        double suma=0;
+        double x=20;
+        suma=suma+x;
+        double y=40;
+        x=x+Math.pow(y,2);
+        suma=suma+x/y;
+        System.out.println("El valor de la suma es: "+suma); 
+    }
+}
